d083f9a14906e8c9fd3bca18635aa05a20f1002c
matiasbiderman <70668471+matiasbiderman@users.noreply.github.com>
2020-09-02 21:43:40 -0300

    Ejercicio 1
    Se agrega el primer ejercicio resuelto

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -10,6 +10,37 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            const int cantnumero = 830;
+            string lista = "";
+            int[] numeros = new int[cantnumero];
+
+            int posicion = 0;
+            do
+            {
+                numeros[posicion] = posicion + 1;
+                if ((numeros[posicion] % 5 == 0) && (numeros[posicion] % 3 == 0))
+                {
+                    lista = numeros[posicion] + "\t - FOOBAR";
+                }
+                else if (numeros[posicion] % 3 == 0)
+                {
+                    lista = numeros[posicion] + "\t - FOO";
+                }
+                else if (numeros[posicion] % 5 == 0)
+                {
+                    lista = numeros[posicion] + "\t - BAR";
+                }
+                else
+                {
+                    lista = numeros[posicion].ToString();
+                }
+                Console.WriteLine(lista);
+                posicion++;
+            } while (posicion < cantnumero);
+
+           
+           /*
+            }*/
         }
     }
 }
