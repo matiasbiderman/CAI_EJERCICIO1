4427e310ab5049e0e81371bcedaaf9bffb986a82
matiasbiderman <70668471+matiasbiderman@users.noreply.github.com>
2020-09-06 12:40:56 -0300

    Ej 6
    Terminado con clases ej 6

---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double result;
+
+            Numero num = new Numero();
+            a = ServValidac.PedirInt("Ingrese primer numero");
+            b = ServValidac.PedirInt("Ingrese segundo numero");
+            
+            result = num.operacion(a, b);
+
+            Console.WriteLine(result);
+
+        }
+    }
+
+    public class Numero
+    {
+        public double operacion(double a, double b)
+        {
+            double resultado;
+            resultado = (a + b) * (a - b);
+            return resultado;
+        }
+    }
+
+}
---
+++ Ejercicio6/ServValidac.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    static class ServValidac
+    {
+        public static double PedirInt(string mensaje)
+        {
+            double valor;
+            do
+            {
+                Console.WriteLine(mensaje);
+                if (!double.TryParse(Console.ReadLine(), out valor))
+                {
+                    valor = - 1;
+                }
+                if (valor < 0)
+                {
+                    Console.WriteLine("ingrese un valor valido");
+                }
+
+            } while (valor < 0);
+            return valor;
+        }
+    }
+}
